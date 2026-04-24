2c2b52370ce8e96f18bef1b9caa70fd1512cdbee
neshkee <skenego@gmail.com>
2026-04-24 17:03:48 +0200

    Napravljena klasa Biblioteka koja implementira BibliotekaInterface

---
+++ src/biblioteka/interfejs/Biblioteka.java
@@ -0,0 +1,40 @@
+package biblioteka.interfejs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import biblioteka.Autor;
+import biblioteka.Knjiga;
+
+public class Biblioteka implements BibliotekaInterface {
+
+	private List<Knjiga> knjige = new ArrayList<>();
+	
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		knjige.add(knjiga);
+
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		knjige.remove(knjiga);
+
+	}
+
+	@Override
+	public List<Knjiga> vratiSveKnjige() {
+		return knjige;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		List<Knjiga> rezultati = new ArrayList<>();
+		for(Knjiga k : knjige) {
+			if(k.getNaslov().toUpperCase().contains(naslov.toUpperCase())) 
+				rezultati.add(k);
+		}
+		return rezultati;
+	}
+
+}
